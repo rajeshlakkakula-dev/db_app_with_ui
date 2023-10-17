3ad76b77be430460aad4bd6c2038e1d4661a6340
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2023-10-17 13:40:43 +0530

    This commit contains database to UI

--- lib/Databases.dart
+++ lib/Databases.dart
@@ -13,7 +13,29 @@ class Databases {
     }, version: 1);
   }
 
+  /**
+   * Always keep the database name and
+   *  table name simple and make sure
+   * datamodel name should match the data provided/given
+   */
   Future<int> insertStudents(List<Students> students) async {
-    return null;
+    int result = 0;
+    final Database db = await initDB();
+
+    for (var student in students) {
+      result = await db.insert('students', student.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+    return result;
   }
+
+  Future<List<Students>> retrieveStudents() async {
+    final Database db = await initDB();
+
+    final List<Map<String, Object>> queryResult = db.query('students') as List<Map<String, Object>>;
+
+    return queryResult.map((e) => Students.fromMap(e)).toList();
+  }
+
+
 }
